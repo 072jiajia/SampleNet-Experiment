6b1519537f009d60c2be39defcc17533b978e8d8
Jia <63368701+072jiajia@users.noreply.github.com>
2020-12-08 17:11:10 +0800

    Update util.py

--- util.py
+++ util.py
@@ -3,25 +3,32 @@ import torch
 import torch.nn.functional as F
 
 
-def cal_loss(pred, gold, smoothing=True):
+def calculate_ACC(pred, labels, threshold=0.5):
+    pred = pred.detach().cpu().numpy().reshape(-1)
+    one_hot_pred = np.zeros(pred.shape)
+    one_hot_pred[pred > threshold] = 1
+    labels = labels.detach().cpu().numpy().reshape(-1)
+    return (one_hot_pred == labels).sum() / len(labels)
+
+def MultiClassBCE(pred, gold, smoothing=False):
     ''' Calculate cross entropy loss, apply label smoothing if needed. '''
-
-    gold = gold.contiguous().view(-1)
-
     if smoothing:
-        eps = 0.2
+        ''' I Don't Actually Sure How to Do Label Smoothing Here '''
+        raise RuntimeError('Do Your Label Smoothing Here')
+        alpha = 0.2
         n_class = pred.size(1)
 
-        one_hot = torch.zeros_like(pred).scatter(1, gold.view(-1, 1), 1)
-        one_hot = one_hot * (1 - eps) + (1 - one_hot) * eps / (n_class - 1)
-        log_prb = F.log_softmax(pred, dim=1)
+        # log_prb = F.logsigmoid(pred)
+        # log_nprb = F.logsigmoid(-pred)
+        # positive = gold * log_prb * ???
+        # negative = (1-gold) * log_nprb * ???
 
-        loss = -(one_hot * log_prb).sum(dim=1).mean()
+        loss = -(positive + negative).sum(dim=1).mean()
     else:
-        loss = F.cross_entropy(pred, gold, reduction='mean')
-
+        loss = F.binary_cross_entropy_with_logits(pred, gold, reduction='none')
+        loss = torch.sum(loss, dim=1)
+        loss = torch.mean(loss, dim=0)
     return loss
-
 
 class IOStream():
     def __init__(self, path):
@@ -34,3 +41,24 @@ class IOStream():
 
     def close(self):
         self.f.close()
+
+
+class AverageMeter(object):
+    """
+    Keeps track of most recent, average, sum, and count of a metric.
+    """
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
